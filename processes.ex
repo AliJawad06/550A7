5ddc5d68af5335f0cd173ef014e9dc9da140d92f
Niyaz Shakeel <niyazshkl@gmail.com>
2023-11-13 18:09:29 -0500

    completed processes.ex & added teams.txt

--- processes.ex
+++ processes.ex
@@ -1,99 +1,108 @@
 defmodule Processes do
+  def eval_code_or_default(code) do
+    try do
+      elem(Code.eval_string(code), 0)
+    rescue
+      _ -> String.to_atom(code)
+    end
+  end
+
   def math do
     receive do
-      {:halt} ->
-        IO.puts("Server1 #{self()}: Halting")
+      givenString ->
+        convertedString = eval_code_or_default(givenString)
 
-        case Process.whereis({:worker, :pid2}) do
-          nil ->
-            IO.puts("Process not found.")
+        case convertedString do
+          {:halt} ->
+            IO.puts("Server1 #{inspect(self())}: Halting")
+            make_request(:PidTwo, :halt)
+            exit(:kill)
 
-          registered_pid ->
-            IO.puts("Found registered process: #{registered_pid}")
-            send(registered_pid, {:halt})
+          :halt ->
+            IO.puts("Server1 #{inspect(self())}: Halting")
+            make_request(:PidTwo, :halt)
+            exit(:kill)
+
+          {:all_done} ->
+            IO.puts("Server1 #{inspect(self())}: Halting")
+            make_request(:PidTwo, :halt)
+            exit(:kill)
+
+          :all_done ->
+            IO.puts("Server1 #{inspect(self())}: Halting")
+            make_request(:PidTwo, :halt)
+            exit(:kill)
+
+          {:add, x, y} ->
+            result = x + y
+            IO.puts("Server1 #{inspect(self())}: Math handling: #{x} + #{y} = #{result}")
+            math()
+
+          {:sub, x, y} ->
+            result = x - y
+            IO.puts("Server1 #{inspect(self())}: Math handling: #{x} - #{y} = #{result}")
+            math()
+
+          {:mult, x, y} ->
+            result = x * y
+            IO.puts("Server1 #{inspect(self())}: Math handling: #{x} * #{y} = #{result}")
+            math()
+
+          {:div, x, y} when y != 0 ->
+            result = x / y
+            IO.puts("Server1 #{inspect(self())}: Math handling: #{x} / #{y} = #{result}")
+            math()
+
+          {:neg, x} ->
+            result = -x
+            IO.puts("Server1 #{inspect(self())}: Math negation handling: #{x} = #{result}")
+            math()
+
+          {:sqrt, x} when x >= 0 ->
+            result = :math.sqrt(x)
+            IO.puts("Server1 #{inspect(self())}: Math sqrt handling: #{x} = #{result}")
+            math()
+
+          _ ->
+            IO.puts("Server1 #{inspect(self())} passed #{givenString} down to the next server!")
+
+            make_request(:PidTwo, givenString)
+            math()
         end
-
-        :ok
-
-      {:add, x, y} ->
-        IO.puts("Server1 (#{self()}): Math handling: #{x} + #{y} = #{x + y}")
-        math()
-
-      {:sub, x, y} ->
-        IO.puts("Server1 (#{self()}): Math handling: #{x} - #{y} = #{x - y}")
-        math()
-
-      {:mult, x, y} ->
-        IO.puts("Server1 (#{self()}): Math handling: #{x} * #{y} = #{x * y}")
-        math()
-
-      {:div, x, y} ->
-        IO.puts("Server1 (#{self()}): Math handling: #{x} / #{y} = #{x / y}")
-        math()
-
-      {:neg, x} ->
-        IO.puts("Server1 (#{self()}): Math negation handling: #{x} = #{-x}")
-        math()
-
-      {:sqrt, x} ->
-        IO.puts("Server1 (#{self()}): Math sqrt handling: #{x} = #{:math.sqrt(x)}")
-        math()
-
-      something_else ->
-        IO.puts("Server1 (#{self()}) passed #{something_else} down to next server!")
-
-        case Process.whereis({:worker, :pid2}) do
-          nil ->
-            IO.puts("Process not found.")
-
-          registered_pid ->
-            IO.puts("Found registered process: #{registered_pid}")
-            send(registered_pid, something_else)
-        end
-
-        math()
     end
   end
 
   def list do
     receive do
-      {:halt} ->
-        IO.puts("Server2 (#{self()}): Halting")
+      :halt ->
+        IO.puts("Server2 #{inspect(self())}: Halting")
+        make_request(:PidThree, :halt)
+        exit(:kill)
 
-        case Process.whereis({:worker, :pid3}) do
-          nil ->
-            IO.puts("Process not found.")
+      givenString ->
+        convertedString = eval_code_or_default(givenString)
 
-          registered_pid ->
-            IO.puts("Found registered process: #{registered_pid}")
-            send(registered_pid, {:halt})
+        case convertedString do
+          [head | tail] when is_integer(head) ->
+            result = Enum.sum([head | tail])
+            IO.puts("Server2 #{inspect(self())}: Handling addition of list elements = #{result}")
+            list()
+
+          [head | tail] when is_float(head) ->
+            result = multiply([head | tail])
+
+            IO.puts(
+              "Server2 #{inspect(self())}: Handling multiplication of list elements = #{result}"
+            )
+
+            list()
+
+          _ ->
+            IO.puts("Server2 #{inspect(self())} passed #{givenString} down to the next server! ")
+
+            make_request(:PidThree, givenString)
+            list()
         end
-
-        :ok
-
-      [head | tail] when is_integer(head) ->
-        result = Enum.sum([head | tail])
-        IO.puts("Server2 (#{self()}): handing addition of list elements = #{result}")
-        list()
-
-      [head | tail] when is_float(head) ->
-        result = multiply([head | tail])
-        IO.puts("Server2 (#{self()}): handing multiplication of list elements = #{result}")
-        list()
-
-      something_else_tho ->
-        IO.puts("Server2 (#{self()}) passed down to next server! #{something_else_tho}")
-
-        case Process.whereis({:worker, :pid3}) do
-          nil ->
-            IO.puts("Process not found.")
-
-          registered_pid ->
-            IO.puts("Found registered process: #{registered_pid}")
-            send(registered_pid, something_else_tho)
-        end
-
-        list()
     end
   end
 
@@ -103,61 +112,59 @@ defmodule Processes do
 
   defp third_one(current_count) do
     receive do
-      {:halt} ->
-        IO.puts("Server3 (#{self()}): Halting")
-        IO.puts("Server3 (#{self()}): #{current_count} unprocessed items!")
-        :ok
+      :halt ->
+        IO.puts("Server3 #{inspect(self())}: Halting")
+        IO.puts("Server3 #{inspect(self())}: #{current_count} unprocessed items!")
+        exit(:kill)
 
-      {:error, x} ->
-        IO.puts("Server3 (#{self()}) - Error: #{x}")
-        third_one(current_count)
+      givenString ->
+        convertedString = eval_code_or_default(givenString)
 
-      something_new ->
-        IO.puts("Server3 (#{self()}) - Not handled: #{something_new}")
-        updated_count = current_count + 1
-        third_one(updated_count)
+        case convertedString do
+          {:error, x} ->
+            IO.puts("Server3 #{inspect(self())}- Error: #{x}")
+            third_one(current_count)
+
+          _ ->
+            IO.puts("Server3 #{inspect(self())}- Not handled: #{givenString}")
+            updated_count = current_count + 1
+            third_one(updated_count)
+        end
     end
   end
 
-  defp multiply([]), do: 1
-  defp multiply([head | tail]), do: head * multiply(tail)
+  def multiply([]), do: 1
 
-  defp make_request(server_id, msg), do: send(server_id, msg)
+  def multiply([head | tail]) do
+    head * multiply(tail)
+  end
+
+  def make_request(server_id, msg) do
+    send(server_id, msg)
+  end
 
   def start do
-    pid1 = spawn(&math/0)
-    IO.puts("Server1 pid is #{inspect(pid1)}")
-    Process.register(pid1, :pid1)
+    firstPID = spawn_link(__MODULE__, :math, [])
+    IO.puts("Server1 pid is #{inspect(firstPID)}")
+    Process.register(firstPID, :PidOne)
 
-    pid2 = spawn(&list/0)
-    IO.puts("Server2 pid is #{inspect(pid2)}")
-    Process.register(pid2, :pid2)
+    secondPID = spawn(__MODULE__, :list, [])
+    IO.puts("Server2 pid is #{inspect(secondPID)}")
+    Process.register(secondPID, :PidTwo)
 
-    pid3 = spawn(&third_one/0)
-    IO.puts("Server3 pid is #{inspect(pid3)}")
-    Process.register(pid3, :pid3)
-
-    main_loop(pid1)
+    thirdPID = spawn(__MODULE__, :third_one, [])
+    IO.puts("Server3 pid is #{inspect(thirdPID)}")
+    Process.register(thirdPID, :PidThree)
+    main_loop(firstPID)
   end
 
-  defp main_loop(pid1) do
-    IO.write(
-      "Note: Since div is a keyword in Erlang,\nyou MUST provide it in this format: {:div, x, y} (enclose :div in atoms)\nEnter operation (ex. {:add, x, y}, [x, y, z]) or type 'all_done' or 'halt' to exit: "
-    )
+  def main_loop(pid1) do
+    IO.puts("Enter operation (ex. {add, X, Y}, [X,Y,Z]) or type 'all_done' or 'halt' to exit: ")
 
-    message = IO.gets("")
+    message = IO.gets("") |> String.trim() |> String.replace_prefix("{", "{:")
 
-    case String.trim(message) do
-      "all_done" ->
-        make_request(pid1, {:halt})
-
-      "halt" ->
-        make_request(pid1, {:halt})
-
-      _ ->
-        make_request(pid1, String.to_existing_atom(message))
-        main_loop(pid1)
-    end
+    make_request(pid1, message)
+    main_loop(pid1)
   end
 end
 
